dcfea151f899126a11301f99b3a36f7423146f19
Pavel Trukhanov <p.trukhanov@hh.ru>
2011-11-20 13:57:34 +0300

    test for tornado >= v2.1~hh3

--- tests/projects/test_app/pages/long_page_request.py
+++ tests/projects/test_app/pages/long_page_request.py
@@ -3,10 +3,12 @@ import time
 import tornado.ioloop
 
 import frontik.handler
+import random
 
 class Page(frontik.handler.PageHandler):
     def get_page(self):
-        self.get_url('localhost:{0}/page/long_page/'.format(self.get_argument('port')),
+        self.get_url('localhost:{0}/test_app/long_page/'.format(self.get_argument('port')),
+                     request_timeout=0.3,
                      callback=self.step2)
 
     def step2(self, xml, response):
